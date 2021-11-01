8c1e20fcc853dc6b47e3df8323fa9f72c3b00d64
sadeq <tsts220g@gmail.com>
2021-11-01 20:24:01 +0330

    hubExm & savedCommands

--- hupExm.sh
+++ hupExm.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 _file=${1:-temp}
 touch ${_file}.txt
-trap "echo 'hup checked' >${_file}.txt" HUP
+trap "echo 'hup checked' >>${_file}.txt" HUP #only if you terminate terminal by mouse
+trap "echo 'term checked' >>${_file}.txt" TERM
+trap "echo 'kill checked' >>${_file}.txt" KILL
+trap "echo 'int checked' >>${_file}.txt" INT
 sleep 20;
 
